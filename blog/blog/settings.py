3004f68edfda7efa8e122e2152b3160835b70bc8
duanyao <1458430198@qq.com>
2023-11-28 21:53:17 +0800

    mysql ready

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -75,11 +75,14 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.mysql', # 数据库引擎
+        'HOST': '127.0.0.1', # 数据库主机
+        'PORT': 3306, # 数据库端口
+        'USER': 'duanyao', # 数据库用户名
+        'PASSWORD': '1234', # 数据库用户密码
+        'NAME': 'blog' # 数据库名字
+    },
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
